e4ee1c72d7966dbbab5f8ba44dfcf165821f1767
rlysa <94913980+rlysa@users.noreply.github.com>
2022-09-22 11:10:55 +0300

    Update tgbot.py
    python-telegram-bot -> aiogram

--- tgbot.py
+++ tgbot.py
@@ -1,34 +1,20 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
-from telegram import ReplyKeyboardMarkup
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+button_instruction = KeyboardButton('Инструкция')
+
+markup = ReplyKeyboardMarkup()
+markup.add(button_instruction)
 
 TOKEN = '5436507493:AAFNMNTR9qJGWJ9YcBEYsYy-blIiHb07hr8'
-REQUEST_KWARGS = {'proxy_url': 'socks5://ip:port'}
 
-reply_keyboard = [['/instruction']]
-markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True)
-
-group = 1
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 
-def start(update, context):
-    update.message.reply_text('''Добро пожаловать!)
-Для получения фотографии отправьте код, указанный на экране фотобудки.''', reply_markup=markup)
-
-
-def instruction(update, context):
-    update.message.reply_text('''1. Сфотографируйтесь в фотобудке школы №1357.
-2. Отправьте код, указанный на экране.''')
-
-
-def main():
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("instruction", instruction))
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply('''Добро пожаловать!)
+Для получения фотографии отправьте код, указанный на экране фотобудки.''',
+                        reply_markup=markup.button_instruction)
