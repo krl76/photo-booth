efa02b6fedba1155f96f3161eefb4ac8a6e86114
rlysa <94913980+rlysa@users.noreply.github.com>
2022-09-23 12:58:09 +0300

    Create tgbot.py
    работающая версия бота на aiogram, кнопка на русском, надо, чтобы после нажатия кнопки приходил ответ

--- tgbot.py
+++ tgbot.py
@@ -1,20 +1,24 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.utils import executor
+
+TOKEN = '5436507493:AAFNMNTR9qJGWJ9YcBEYsYy-blIiHb07hr8'
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
 button_instruction = KeyboardButton('Инструкция')
 
-markup = ReplyKeyboardMarkup()
+markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 markup.add(button_instruction)
-
-TOKEN = '5436507493:AAFNMNTR9qJGWJ9YcBEYsYy-blIiHb07hr8'
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply('''Добро пожаловать!)
 Для получения фотографии отправьте код, указанный на экране фотобудки.''',
-                        reply_markup=markup.button_instruction)
+                        reply_markup=markup)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
