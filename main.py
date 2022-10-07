fe635d8f217dfa2da422b2a34414e94562906984
krl76 <99721154+krl76@users.noreply.github.com>
2022-10-07 15:12:07 +0300

    вроде начало работать, но я думаю можно лучше

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request
 import base64
+from time import sleep
 
 app = Flask(__name__, static_folder="static")
 
@@ -14,19 +15,35 @@ def second():
     return render_template('second.html')
 
 
-@app.route("/send", methods=["POST"])
+# @app.route("/image", methods=["GET", "POST"])
+# def upload_image():
+#     if request.method == 'POST':
+#         with open('static/images/image.txt', 'w') as file:
+#             img = request.form['image']
+#             file.write(img)
+#         with open('static/images/image.txt', 'rb') as file:
+#             with open('static/images/image.png', 'wb') as file2:
+#                 img_b64 = file.read()
+#                 file2.write(base64.b64decode(img_b64))
+#         sleep(0.5)
+#         return redirect(url_for('send'))
+
+
+@app.route("/send", methods=["GET", "POST"])
 def third():
-    with open('static/images/image.txt', 'w') as file:
-        img = request.form['image']
-        file.write(img)
-    with open('static/images/image.txt', 'rb') as file:
-        with open('static/images/image.png', 'wb') as file2:
-            img_b64 = file.read()
-            file2.write(base64.b64decode(img_b64))
+    if request.method == 'POST':
+        with open('static/images/image.txt', 'w') as file:
+            img = request.form['image']
+            file.write(img)
+        with open('static/images/image.txt', 'rb') as file:
+            with open('static/images/image.png', 'wb') as file2:
+                img_b64 = file.read()
+                file2.write(base64.b64decode(img_b64))
     # with open('static/images/image.txt', 'rb') as file:
     #     with open('static/images/image.png', 'wb') as file2:
     #         img_b64 = file.read()
     #         file2.write(base64.b64decode(img_b64))
+    sleep(1)
     return render_template('third.html')
 
 
