a4af03f4e0e235623b0de68d0504b323df284567
krl76 <99721154+krl76@users.noreply.github.com>
2022-10-21 13:09:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-
+import qrcode
 from flask import Flask, render_template, redirect, url_for, request
 import base64
 from time import sleep
@@ -9,27 +9,13 @@ app = Flask(__name__, static_folder="static")
 
 
 @app.route("/")
-def first():
+def greetings_screen():
     return render_template('main.html')
 
 
 @app.route("/camera")
-def second():
+def camera_screen():
     return render_template('second.html')
-
-
-# @app.route("/image", methods=["GET", "POST"])
-# def upload_image():
-#     if request.method == 'POST':
-#         with open('static/images/image.txt', 'w') as file:
-#             img = request.form['image']
-#             file.write(img)
-#         with open('static/images/image.txt', 'rb') as file:
-#             with open('static/images/image.png', 'wb') as file2:
-#                 img_b64 = file.read()
-#                 file2.write(base64.b64decode(img_b64))
-#         sleep(0.5)
-#         return redirect(url_for('send'))
 
 
 @app.route("/send", methods=["POST"])
@@ -47,6 +33,19 @@ def upload_image():
     except Exception:
         return json.dumps({'error': 'При загрузке произошла ошибка'})
     return json.dumps({'success': 'ok', 'img': path})
+
+
+@app.route("/qr", methods=["GET"])
+def qr_code():
+    try:
+        tgbot_name = 'photobooth1357_bot'
+        tgbot_link = f'https://t.me/{tgbot_name}'
+        tgbot_qr = qrcode.make(tgbot_link)
+        path_tgbot_qr = '/static/images/telegram-qrcode.png'
+        tgbot_qr.save('/static/images/telegram-qrcode.png')
+    except Exception:
+        return json.dumps({'error': 'При загрузке произошла ошибка'})
+    return json.dumps({'success': 'ok', 'tglink': path_tgbot_qr})
 
 
 if __name__ == '__main__':
