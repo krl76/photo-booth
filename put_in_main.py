f246342f9c1df685253fa8f0d480005e30ae1047
rlysa <94913980+rlysa@users.noreply.github.com>
2022-10-23 22:08:41 +0300

    Update put_in_main.py

--- put_in_main.py
+++ put_in_main.py
@@ -1,3 +1,17 @@
+from db_data import db_session
+from db_data.photos import Photo
+from random import choices
+
+
+db = db_session.create_session()
+photo = Photo()
+photo.photo = 'img'
+generate_code = choices(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], k=8)
+photo.code = generate_code
+db.add(photo)
+db.commit()
+       
 def main():
     db_session.global_init('db/photo-booth.sqlite')
     app.run()
+
