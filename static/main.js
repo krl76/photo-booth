83f30bd87fffcd4677eaf4f1d06f5171c9379f97
krl76 <k.kornilov2005@yandex.ru>
2022-10-31 22:40:53 +0300

    Revert "some update"
    This reverts commit 9b8a524c

--- static/main.js
+++ static/main.js
@@ -2,5 +2,5 @@ transition.addEventListener('click', function(){
     var childWindow = window.open('camera');
     setTimeout(function () {
     childWindow.close();
-    }, 180000);
+    }, 5000);
 });
