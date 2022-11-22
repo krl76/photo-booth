23361f1a8fc216aa286c378c6c1ff86061e96787
rlysa <margarita.lysacheva@yandex.ru>
2022-11-22 21:39:17 +0300

    + correct mistake in chats.py

--- db_data/chats.py
+++ db_data/chats.py
@@ -5,7 +5,7 @@ from .db_session import SqlAlchemyBase
 class Chat(SqlAlchemyBase):
     __tablename__ = 'Chats'
 
-    id = sqlalchemy.Column(sqlalchemy.String, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     chat_id =sqlalchemy.Column(sqlalchemy.String, unique=True)
     data = sqlalchemy.Column(sqlalchemy.DateTime)
     count_photo = sqlalchemy.Column(sqlalchemy.Float)
