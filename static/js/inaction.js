f308db1b2d514173c677545898b33e8ea7f0ca0e
krl76 <k.kornilov2005@yandex.ru>
2023-01-18 14:13:25 +0300

    модальное окно - таймер на второй-третий скрин

--- static/js/inaction.js
+++ static/js/inaction.js
@@ -1,21 +1,36 @@
+var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), 'focus');
+
 let time = 0;
 $(document).ready(function () {
     //здесь функция срабатывает раз в минуту, можно настроить как вам удобно
-    let interval = setInterval(idleTimer, 60000);
+    let interval = setInterval(idleTimer, 1000);
     //тут можно добавлять все события которые не должны срабатывать(нажатие на клавиши, ресайз и т.д.)
     $(this).on('mousemove', function (e) {
     time = 0;
     });
 });
-//тут задаем время простоя, сейчас стоит 3 минуты
+
 function idleTimer() {
     time = time + 1;
     if (time > 2) {
         //здесь нужное вам действие на простой
-        /*var isHere = confirm("Вы бездействовали на странице более 3х минут, желаете продолжить?");*/
-        history.back();
-        /*if (!(isHere)) {
+        myModal.show();
+        let no = document.querySelector('#no');
+        let yes = document.querySelector('#yes');
+        yes.addEventListener('click', function() {
             history.back();
-        }*/
-    }
-}
+        });
+        no.addEventListener('click', function() {
+            myModal.hide();
+        });
+         timerload(15)
+    };
+};
+
+function timerload(t){
+    if (t == 0){
+      history.back();
+    };
+    document.getElementById('no').innerHTML = 'Нет (' + t + ')'
+    setTimeout(() =>  timerload(t - 1), 1000);
+};
