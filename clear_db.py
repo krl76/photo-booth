de837baa1d4f3d0d96072b619381d229db40149a
rlysa <margarita.lysacheva@yandex.ru>
2023-02-12 23:43:12 +0300

    ассинхрончик сделан

--- clear_db.py
+++ clear_db.py
@@ -1,12 +1,17 @@
 import sqlite3
 import datetime
+import os
 
 
 def delete_photos():
     connection = sqlite3.connect('db/photo-booth.sqlite')
     cursor = connection.cursor()
     delete = cursor.execute(
-        f'''DELETE FROM photos WHERE time<"{datetime.datetime.now() - datetime.timedelta(minutes=10)}"''').fetchall()
+        f'''SELECT photo FROM photos WHERE time<"{datetime.datetime.now() - datetime.timedelta(minutes=15)}"''').fetchall()
+    for photo in delete:
+        os.remove(photo[0])
+    delete = cursor.execute(
+        f'''DELETE FROM photos WHERE time<"{datetime.datetime.now() - datetime.timedelta(minutes=15)}"''').fetchall()
     connection.commit()
     connection.close()
 
@@ -18,4 +23,9 @@ def delete_statistics():
         f'''DELETE FROM statistics WHERE time<"{datetime.datetime.now() - datetime.timedelta(weeks=1)}"''').fetchall()
     connection.commit()
     connection.close()
+
+
+# if __name__ == '__main__':
+#     delete_photos()
+#     delete_statistics()
 
