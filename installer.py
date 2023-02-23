3ac05cc53bb067f8548ceb9499b5af52297af42b
rlysa <margarita.lysacheva@yandex.ru>
2023-02-24 00:09:44 +0300

    в зависимости от операционки выводится либо pip, либо pip3

--- installer.py
+++ installer.py
@@ -1,6 +1,9 @@
 import os
+from platform import system
 
 print('Начинаю установку библиотек')
-print(os.path)
-os.system('pip install -r requirements.txt ')
+if system() == 'Windows':
+    os.system('pip install -r requirements.txt ')
+elif system() == 'Linux':
+    os.system('pip3 install -r requirements.txt ')
 print('Завершение настроек. Для старта запустите main.py и tgbot.py')
